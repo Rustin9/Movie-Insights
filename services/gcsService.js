7d606065ae0deb3a1d2ca825c2af6e5c0c576fa5
Rustin C <rustinc9@gmail.com>
2026-04-09 17:53:57 -0400

    fix: convert ETL to CommonJS-4

--- services/gcsService.js
+++ services/gcsService.js
@@ -1,13 +1,17 @@
 import { Storage } from "@google-cloud/storage";
 
 const storage = new Storage({
-  keyFilename: "movie-insights-sa-key.json",
+  credentials: JSON.parse(process.env.GOOGLE_CREDENTIALS_JSON),
 });
 
 const bucketName = "ai-movie-insights-data";
 
 export async function getMoviesFromGCS() {
-  const file = storage.bucket(bucketName).file("gold/movies.json");
+  const file = storage
+    .bucket(bucketName)
+    .file("gold/movies.json");
+
   const contents = await file.download();
+
   return JSON.parse(contents.toString());
 }
